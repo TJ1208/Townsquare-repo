44aaf5b32e5e8b2f2a9e0da488c006c294d6fdc1
Taylor <TaylorJ1208@yahoo.com>
2022-12-15 11:00:43 -0500

    adds request notification to navbar

--- townsquare-frontend/src/app/components/navbar/navbar.component.ts
+++ townsquare-frontend/src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,12 @@
+import { NgLocalization } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 import { User } from 'src/app/models/User';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { RequestService } from 'src/app/services/request/request.service';
 import { UserService } from 'src/app/services/user/user.service';
+import { Request } from '../../models/Request';
 
 @Component({
   selector: 'app-navbar',
@@ -16,12 +20,19 @@ export class NavbarComponent implements OnInit {
   userNames: User[] = [];
   keyword: string = "firstName";
   searchWord: string = "";
+  requests: Request[] = [];
+  requests$ = new BehaviorSubject<Request[]>([]);
+  cast = this.requests$.asObservable();
   constructor(private router: Router, private authService: AuthService,
-    private userService: UserService) { }
+    private userService: UserService, private requestService: RequestService) { }
 
   ngOnInit(): void {
     this.getCurrentUserImage();
     this.getAllUsers();
+    this.getFriendRequests();
+    this.cast.subscribe((requests: Request[]) => {
+      this.requests = requests;
+    })
   }
 
   getAllUsers(): void {
@@ -30,19 +41,38 @@ export class NavbarComponent implements OnInit {
       this.users = users;
     })
   }
+
   getCurrentUserImage(): void {
     this.userService.getUserById(parseInt(this.userId)).subscribe((user) => {
       this.userImg = user.profileImg;
     })
   }
 
+  retrieveRequests(requests: Request[]): void {
+    this.requests$.next(requests);
+  }
+
+  getFriendRequests(): void {
+    this.requestService.getAllUserRequests(parseInt(this.userId)).subscribe((requests: any) => {
+      this.cast = requests;
+      this.retrieveRequests(requests);
+    })
+  }
+
   selectEvent(user: User): void {
     localStorage.setItem("visitedUser", user.userId.toString());
-    this.router.navigate(['/user']);
     if (this.router.url == '/user') {
       location.reload();
     }
+    this.router.navigate(['/user']);
+  }
 
+  selectEventPicture(userId: number): void {
+    localStorage.setItem("visitedUser", userId.toString());
+    if (this.router.url == '/user') {
+      location.reload();
+    }
+    this.router.navigate(['/user']);
   }
 
   updateSearchWord(search: string): void {
