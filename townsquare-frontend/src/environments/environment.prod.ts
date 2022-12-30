87972c0402f900eabf68e2936f6729f068e97da6
Taylor <TaylorJ1208@yahoo.com>
2022-12-30 12:15:48 -0500

    adds socketUrl prod mode

--- townsquare-frontend/src/environments/environment.prod.ts
+++ townsquare-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: "https://townsquare-frontend.azurewebsites.net/"
+  baseUrl: "https://townsquare-frontend.azurewebsites.net/",
+  socketUrl: 'https://townsquare-frontend.azurewebsites.net'
 };
